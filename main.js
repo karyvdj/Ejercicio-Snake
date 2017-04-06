0ade2e6b0b79506834e229988c99335c7402da1c
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-06 17:28:18 -0500

    Movimiento por tecla de serpiente

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
   //alert("hola");
 var tabla = document.getElementById('tabla');
 var serpiente = document.getElementById('serpiente');
-var posicionInicialX = 250;
-var posicionInicialY = 250;
-var avanzarX = 20;
-var avanzarY = 20;
+
+document.addEventListener("keydown", avanzarSerpiente);
+
+var posicionX = 0;
+var posicionY= 0;
+var movimiento = 20;
+
+
+function avanzarSerpiente() {
+  switch (event.keyCode){ //codigo ASCCI
+      case 37 : //IZQUIERDA
+        posicionX = posicionX - movimiento;
+        if (posicionX < 0){
+           gameOver();
+        } else{
+          serpiente.style.marginLeft = posicionX +"px" ;
+        }
+      break;
+      case 39 : //DERECHA
+        posicionX = posicionX + movimiento;
+        if (posicionX > 480){
+          gameOver();
+        } else{
+          serpiente.style.marginLeft = posicionX +"px" ;
+        }
+      break;
+      case 38 : //ARRIBA
+      // alert(event.key);
+        posicionY = posicionY - movimiento;
+        if (posicionY < 0){
+          gameOver();
+        } else{
+          serpiente.style.marginTop = posicionY +"px" ;
+        }
+      break;
+      case 40 : //ABAJO
+        posicionY = posicionY + movimiento;
+        if (posicionY > 480){
+           gameOver();
+        } else{
+          serpiente.style.marginTop = posicionY +"px" ;
+        }
+      break;
+      default:
+        // alert("Pulsa solo las flechas");
+  }
+}
+
+function gameOver() {
+  alert("GAME OVER");
+}

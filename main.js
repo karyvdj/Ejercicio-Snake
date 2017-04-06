4a441e8f764fa2fff2bf8c1c050adf9b27d63670
OfeliaGarc <ofee.50.og@gmail.com>
2017-04-06 11:43:55 -0500

    Estructura HTML/enlace de js y css

---
+++ main.js
@@ -0,0 +1,7 @@
+  //alert("hola");
+var tabla = document.getElementById('tabla');
+var serpiente = document.getElementById('serpiente');
+var posicionInicialX = 250;
+var posicionInicialY = 250;
+var avanzarX = 20;
+var avanzarY = 20;
